4c3d9a15064b55316876e8fd1a0f2b9ffda96316
alumno <alumno@Instituto.local>
2025-09-17 08:28:49 -0300

    aa

--- ApINasa.js
+++ ApINasa.js
@@ -1,45 +1,13 @@
-let API = 'PnfTdhzcF0kCOilpRqi7iUa4QMBmxUoGQ9uDPUPp';
-let imagen = document.getElementById('imagen');
-let titulo = document.getElementById('titulo');
-let fechaDiv = document.getElementById('fecha');
-let dia = document.getElementById('dia');
-let fechaBtn = document.getElementById('fechaBtn');
-let aleatorioBtn = document.getElementById('aleatorioBtn');
-let fecha = document.getElementById('fecha');
-let aleatorio = document.getElementById('aleatorio');
-
-dia.onclick = function() {
-  fetch('https://api.nasa.gov/planetary/apod?api_key=' + API)
-    .then(function(x){ return x.text(); })
-    .then(function(txt){
-      let f = JSON.parse(txt); 
-      imagen.src = f.url;
-      titulo.textContent = f.title;
-      fechaDiv.textContent = f.date;
-    });
-};
-
-fechaBtn.onclick = function() {
-  let f = fecha.value;
-  fetch('https://api.nasa.gov/planetary/apod?api_key=' + API + '&date=' + f)
-    .then(function(x){ return x.text(); })
-    .then(function(txt){
-      let f = JSON.parse(txt);
-      imagen.src = f.url;
-      titulo.textContent = f.title;
-      fechaDiv.textContent = f.date;
-    });
-};
-
-aleatorioBtn.onclick = function() {
-  let n = aleatorio.value;
-  fetch('https://api.nasa.gov/planetary/apod?api_key=' + API + '&count=' + n)
-    .then(function(x){ return x.text(); })
-    .then(function(txt){
-      let fs = JSON.parse(txt);
- 
-      imagen.src = fs[0].url;
-      titulo.textContent = fs[0].title;
-      fechaDiv.textContent = fs[0].date;
-    });
-};
+//elegir foto entre dos fechas elegidas por el usuario
+let key = 'PnfTdhzcF0kCOilpRqi7iUa4QMBmxUoGQ9uDPUPp';
+let imagenFecha = document.querySelector('#imagenFecha')
+let botonFecha = document.querySelector('#botonFecha')
+let fechaUsuario = document.querySelector('#fecha')
+botonFecha.onclick = function (){
+  console.log (fechaUsuario)
+  fetch('https://api.nasa.gov/planetary/apod?api_key=PnfTdhzcF0kCOilpRqi7iUa4QMBmxUoGQ9uDPUPp&date=2020-05-01')
+  .then(res => res.json())
+  .then(fotoFecha => {
+imagenFecha.src = fotoFecha.hdurl
+  })
+}
