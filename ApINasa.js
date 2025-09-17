b4e21eb56044e76b80387dc3e10c970713232c1d
alumno <alumno@Instituto.local>
2025-09-17 10:44:55 -0300

    aa

--- ApINasa.js
+++ ApINasa.js
@@ -1,13 +1,15 @@
-//elegir foto entre dos fechas elegidas por el usuario
-let key = 'PnfTdhzcF0kCOilpRqi7iUa4QMBmxUoGQ9uDPUPp';
-let imagenFecha = document.querySelector('#imagenFecha')
-let botonFecha = document.querySelector('#botonFecha')
-let fechaUsuario = document.querySelector('#fecha')
-botonFecha.onclick = function (){
-  console.log (fechaUsuario)
-  fetch('https://api.nasa.gov/planetary/apod?api_key=PnfTdhzcF0kCOilpRqi7iUa4QMBmxUoGQ9uDPUPp&date=2020-05-01')
-  .then(res => res.json())
-  .then(fotoFecha => {
-imagenFecha.src = fotoFecha.hdurl
-  })
+let key =`b0guUnf4BeGhlql7UYesZsz739sYAFvYIyEIbXge`
+
+let botonFecha = document.querySelector("#botonFecha")
+let imagenFecha = document.querySelector("#imagenFecha")
+let fecha = document.querySelector("#fecha")
+
+botonFecha.onclick = function() {
+    console.log(fecha)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fecha.value}`)
+    .then(res => res.json())
+    .then(fotoFecha => {
+        imagenFecha.src = fotoFecha.hdurl
+        titulo.textContent = fotoFecha.title
+    })
 }
