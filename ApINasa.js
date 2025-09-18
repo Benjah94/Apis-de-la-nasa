00b5d102f546bb2837ec4c801083e5bd464144dc
alumno <alumno@Instituto.local>
2025-09-18 11:22:37 -0300

    aa

--- ApINasa.js
+++ ApINasa.js
@@ -2,11 +2,13 @@ let key =`b0guUnf4BeGhlql7UYesZsz739sYAFvYIyEIbXge`
 
 let botonFecha = document.querySelector("#botonFecha")
 let imagenFecha = document.querySelector("#imagenFecha")
-let fecha = document.querySelector("#fecha")
+let fecha1 = document.querySelector("#fecha1")
+let fecha2 = document.querySelector("#fecha2")
 
 botonFecha.onclick = function() {
     console.log(fecha)
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fecha.value}`)
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=${fecha1.value}&end_date=${fecha2.value}`)
     .then(res => res.json())
     .then(fotoFecha => {
         imagenFecha.src = fotoFecha.hdurl
